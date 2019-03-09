a06a3814ac5e0148993367f7093663ffe8125d1a
Abel Lopez Paniagua <topx777@gmail.com>
2019-03-08 22:57:43 -0600

    Ingrediente

--- ChocolateriaForms/ChocolateriaForms/Ingrediente.cs
+++ ChocolateriaForms/ChocolateriaForms/Ingrediente.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace ChocolateriaForms
 {
-    class Turno
+    class Ingrediente
     {
-        public static string[] ObtenerTurno(int ID)
+
+        //Metodo para ver solo una sucursal especifica
+        public static string[] ObtenerIngrediente(int ID)
         {
             string[] datos = new string[2];
 
-            string query = @"SELECT * FROM turno WHERE id_turno = @ID LIMIT 1";
+            string query = @"SELECT * FROM sucursal WHERE id_sucursal = @ID LIMIT 1";
 
             MySqlCommand cmd = new MySqlCommand(query, Conexion.obtenerConexion());
             cmd.Parameters.AddWithValue("ID", ID);
@@ -37,9 +39,10 @@ namespace ChocolateriaForms
             }
         }
 
-        public static DataTable ListaTurnos()
+        //Metodo para listar todas las sucursales
+        public static DataTable ListaSucursales()
         {
-            string query = @"SELECT * FROM turno";
+            string query = @"SELECT * FROM sucursal";
 
             MySqlDataAdapter adapter = new MySqlDataAdapter(query, Conexion.obtenerConexion());
 
@@ -50,10 +53,11 @@ namespace ChocolateriaForms
         }
 
 
-        public static int CrearTurno(string Nombre)
+        //Metodo para crear una nueva sucursal
+        public static int CrearSucursal(string Nombre)
         {
-            string query1 = @"SELECT nombre FROM turno 
-                            WHERE nombre = @Nombre";
+            string query1 = @"SELECT nombre_sucursal FROM sucursal 
+                            WHERE nombre_sucursal = @Nombre";
 
             MySqlCommand cmd1 = new MySqlCommand(query1, Conexion.obtenerConexion());
             cmd1.Parameters.AddWithValue("Nombre", Nombre);
@@ -65,7 +69,7 @@ namespace ChocolateriaForms
                 return 0;
             }
 
-            string query = @"INSERT INTO turno(nombre) 
+            string query = @"INSERT INTO sucursal(nombre_sucursal) 
                             VALUES(@Nombre)";
             MySqlCommand cmd = new MySqlCommand(query, Conexion.obtenerConexion());
 
@@ -85,10 +89,11 @@ namespace ChocolateriaForms
         }
 
 
-        public static int ModificarTurno(int ID, string Nombre)
+        //Metodo para modificar una sucursal
+        public static int ModificarSucursal(int ID, string Nombre)
         {
-            string query1 = @"SELECT nombre FROM turno
-                            WHERE nombre = @Nombre AND id_turno != @ID";
+            string query1 = @"SELECT nombre_sucursal FROM sucursal
+                            WHERE nombre_sucursal = @Nombre AND id_sucursal != @ID";
 
             MySqlCommand cmd1 = new MySqlCommand(query1, Conexion.obtenerConexion());
             cmd1.Parameters.AddWithValue("Nombre", Nombre);
@@ -101,8 +106,8 @@ namespace ChocolateriaForms
                 return 0;
             }
 
-            string query = @"UPDATE turno SET nombre = @Nombre 
-                            WHERE id_turno = @ID";
+            string query = @"UPDATE sucursal SET nombre_sucursal = @Nombre 
+                            WHERE id_sucursal = @ID";
 
             MySqlCommand cmd = new MySqlCommand(query, Conexion.obtenerConexion());
             cmd.Parameters.AddWithValue("Nombre", Nombre);
@@ -122,9 +127,10 @@ namespace ChocolateriaForms
         }
 
 
-        public static int EliminarTurno(int ID)
+        //Metodo para eliminar una sucursal
+        public static int EliminarSucursal(int ID)
         {
-            string query = @"DELETE FROM turno WHERE id_turno = @ID";
+            string query = @"DELETE FROM sucursal WHERE id_sucursal = @ID";
 
             MySqlCommand cmd = new MySqlCommand(query, Conexion.obtenerConexion());
             cmd.Parameters.AddWithValue("ID", ID);
